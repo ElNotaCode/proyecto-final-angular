a27aafce46c053d46fcf789f40ac337d36142046
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-06-11 11:56:28 +0200

    form-candidate-position

--- entrevistaFinal/src/app/admin/components/formularios/form-candidate-position/form-candidate-position.component.ts
+++ entrevistaFinal/src/app/admin/components/formularios/form-candidate-position/form-candidate-position.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-candidate-position',
   templateUrl: './form-candidate-position.component.html',
-  styleUrls: ['./form-candidate-position.component.css']
+  styleUrls: ['./form-candidate-position.component.css'],
 })
 export class FormCandidatePositionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  fkCandidate: any;
+  fkPosition: any;
 
+  validarFkCandidate: boolean = true;
+  validarFkPosition: boolean = true;
+
+  enviarFormulario() {}
 }
