4654e1b1b7d170e54c45de3249d636c283ff8420
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-06-11 12:18:02 +0200

    form-hr-user implementado

--- entrevistaFinal/src/app/admin/components/formularios/form-hr-user/form-hr-user.component.ts
+++ entrevistaFinal/src/app/admin/components/formularios/form-hr-user/form-hr-user.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-hr-user',
   templateUrl: './form-hr-user.component.html',
-  styleUrls: ['./form-hr-user.component.css']
+  styleUrls: ['./form-hr-user.component.css'],
 })
 export class FormHrUserComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  username: string = '';
 
+  validarUsername: boolean = true;
 }
