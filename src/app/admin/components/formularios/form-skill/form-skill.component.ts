589cdf08a3c5e72bbee9bc7fcd903d4020044a2f
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-06-15 18:43:05 +0200

    create skill completado pero pasa un valor nulo al crear

--- src/app/admin/components/formularios/form-skill/form-skill.component.ts
+++ src/app/admin/components/formularios/form-skill/form-skill.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillService } from 'src/app/admin/services/s-skill/skill.service';
 
 @Component({
   selector: 'app-form-skill',
@@ -6,14 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-skill.component.css'],
 })
 export class FormSkillComponent implements OnInit {
-  constructor() {}
+  constructor(private skillService: SkillService) {}
 
   ngOnInit(): void {}
 
-  nombreSkill: string = '';
-  validarNombre: boolean = true;
+  //esta variable se usa para indicar que se ha creado la skill
+  isSkillAdded = false;
 
-  enviarFormulario() {
-    alert('Este botón no envia nada.');
+  //objeto de la skill
+  skill = {
+    nombreSkill: '',
+  };
+
+  //esta variable indicará que se tiene que introducir un nombre válido
+  validarNombre: boolean = false;
+
+  //declaramos esta función en void
+  addSkill(): void {
+    const data = {
+      nombreSkill: this.skill.nombreSkill,
+    };
+
+    //control
+    if (!data.nombreSkill) {
+      //mostramos el mensaje de validación
+      this.validarNombre = true;
+      //importante poner el return para que pare la función
+      return;
+    } else {
+      this.skillService.createSkill(data).subscribe(
+        (response) => {
+          console.log(response);
+          this.isSkillAdded = true;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  //esta función es para el botón que se mostrará al crear una skill
+  newSkill() {
+    //resetea el objeto y la var si está añadida
+    this.isSkillAdded = false;
+
+    this.skill = {
+      nombreSkill: '',
+    };
   }
 }
