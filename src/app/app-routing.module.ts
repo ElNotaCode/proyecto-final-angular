4882da050cbd7915ac782eee5ca0ad1fe6fc32da
Marc Lopez <marclopezmarco469@gmail.com>
2022-06-15 13:00:17 +0200

    bug arreglado
    bug arreglado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AsideComponent } from './admin/aside/aside.component';
 import { TableSkillComponent } from './admin/components/listas/table-skill/table-skill.component';
+import { CandidaturaComponent } from './public/candidatura/candidatura.component';
 
 const routes: Routes = [
   {
+    path: 'candidatura',
+    component: CandidaturaComponent,
+  },
+  {
     path: 'skills',
     component: TableSkillComponent,
-  },
+  }
+
 ];
 
 @NgModule({
