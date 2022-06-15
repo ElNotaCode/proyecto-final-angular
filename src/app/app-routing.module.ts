8d334e437d33d4fee50380c82c374af68de788c5
Marc Lopez <marclopezmarco469@gmail.com>
2022-06-15 16:20:11 +0200

    lista terminada cadnidateposition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,41 @@
+
+
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AsideComponent } from './admin/aside/aside.component';
+import { TableCandidatePositionComponent } from './admin/components/listas/table-candidate-position/table-candidate-position.component';
+import { TableHrUserComponent } from './admin/components/listas/table-hr-user/table-hr-user.component';
 import { TableSkillComponent } from './admin/components/listas/table-skill/table-skill.component';
 import { CandidaturaComponent } from './public/candidatura/candidatura.component';
+import { HomeComponent } from './public/home/home.component';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'candidatura',
     component: CandidaturaComponent,
   },
   {
+    path: 'candidateposition',
+    component: TableCandidatePositionComponent,
+  },
+  {
     path: 'skills',
     component: TableSkillComponent,
-  }
-
+  },
+  {
+    path: 'hrUser',
+    component: TableHrUserComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
@@ -21,3 +43,13 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+
+
+
+
+
+
+
+
+
+
