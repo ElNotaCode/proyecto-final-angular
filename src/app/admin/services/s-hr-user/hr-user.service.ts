9249779236862d51ed2196a067940510cf82d736
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2022-06-16 11:18:23 +0200

    CREATE HRUser terminado

--- src/app/admin/services/s-hr-user/hr-user.service.ts
+++ src/app/admin/services/s-hr-user/hr-user.service.ts
@@ -1,19 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-/*
-* @Author Borja Montseny
-*/
+/**
+ * @Author Borja Montseny
+ */
+
+const apiURL: string = 'http://localhost:8181/api/hrUsers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HrUserService {
-
-  // TODO: Pasar a la URL de Heroku cuando funcione
-
-  apiURL: string = 'http://localhost:8181/api/hrUsers';
+  //TODO: Pasar a la URL de heroku cuando funcione
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   // importamos HttpClient y lo pasamos
   constructor(private httpClient: HttpClient) {}
@@ -21,8 +21,26 @@ export class HrUserService {
   // list() nos mostrará todos los registros
   listHrUser(): Observable<any>{
     // TODO: Añadir handle error
-    return this.httpClient.get(this.apiURL);
+    return this.httpClient.get(apiURL);
   }
 
+  createHrUser(data: any): Observable<any> {
+    return this.httpClient.post(apiURL, data);
+  }
+
+  //R
+  readHrUser(id: any): Observable<any> {
+    return this.httpClient.get('${apiURL}/${id}');
+  }
+
+  //U
+  updateHrUser(id: any, data: any): Observable<any> {
+    return this.httpClient.put('${apiURL}/${id}', data);
+  }
+
+  //D
+  deleteHrUser(id: any): Observable<any> {
+    return this.httpClient.delete('${apiURL}/${id}');
+  }
 
 }
