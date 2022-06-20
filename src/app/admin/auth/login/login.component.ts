c1d702c3ffe3d8d7a88f99650f3d1381ef6f7951
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-06-20 13:40:30 +0200

    button logout

--- src/app/admin/auth/login/login.component.ts
+++ src/app/admin/auth/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
     const { username, password } = this.form;
 
     this.authService.login(username, password).subscribe((data) => {
-      this.tokenStorage.saveToken(data.accessToken);
+      this.tokenStorage.saveToken(data.token);
       this.tokenStorage.saveUser(data);
 
       this.isLoginFailed = false;
