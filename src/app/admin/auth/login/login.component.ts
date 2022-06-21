7bdeb40f8b8d0da7b2589965d5543e0cbaf85ebe
Marc Lopez <marclopezmarco469@gmail.com>
2022-06-21 12:28:18 +0200

    pop up

--- src/app/admin/auth/login/login.component.ts
+++ src/app/admin/auth/login/login.component.ts
@@ -25,16 +25,16 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
 
-    const sign_in_btn:any = document.querySelector('#sign-in-btn');
-    const sign_up_btn:any = document.querySelector('#sign-up-btn');
-    const container:any = document.querySelector('.container');
+    const sign_in_btn: any = document.querySelector("#sign-in-btn");
+    const sign_up_btn: any = document.querySelector("#sign-up-btn");
+    const container: any = document.querySelector(".container");
 
-    sign_up_btn.addEventListener('click', () => {
-      container.classList.add('sign-up-mode');
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
     });
 
-    sign_in_btn.addEventListener('click', () => {
-      container.classList.remove('sign-up-mode');
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
     });
 
     if (this.tokenStorage.getToken()) {
