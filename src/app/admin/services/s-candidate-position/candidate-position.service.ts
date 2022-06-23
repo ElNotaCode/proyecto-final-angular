db6588b560d2b1b7237332d25bb8653f8b1d236e
Marc Lopez <marclopezmarco469@gmail.com>
2022-06-23 12:21:10 +0200

    r

--- src/app/admin/services/s-candidate-position/candidate-position.service.ts
+++ src/app/admin/services/s-candidate-position/candidate-position.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+<<<<<<< HEAD
 
 
 /**
@@ -10,17 +11,37 @@ import { Observable } from 'rxjs';
 
 const apiUrl: string = 'http://localhost:8181/api/candidatePosition';
 
+=======
+const apiUrl: string = 'http://localhost:8181/api/candidatePosition';
+>>>>>>> master
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class CandidatePositionService {
+  // TODO: Pasar a la URL de heroku cuando funcione
 
+<<<<<<< HEAD
 //importamos HttpClient y lo pasamos como parametro al constructor
  constructor(private httpClient: HttpClient) {}
+=======
+  //importamos HttpClient y lo pasamos como parametro al constructor
+  constructor(private httpClient: HttpClient) {}
 
- //list() nos mostrará todos los registros
+  //list() nos mostrará todos los registros
 
+  listCandidatePosition(): Observable<any> {
+    //TODO: Añadir handle error
+    return this.httpClient.get(apiUrl);
+  }
+>>>>>>> master
+
+  //R
+  readCandidatePosition(id: any): Observable<any> {
+    return this.httpClient.get(`${apiUrl}/${id}`);
+  }
+
+<<<<<<< HEAD
  listCandidatePosition(): Observable<any>{
   //TODO: Añadir handle error
   return this.httpClient.get(apiUrl);
@@ -34,4 +55,15 @@ export class CandidatePositionService {
     return this.httpClient.post(apiUrl, data);
   }
 
+=======
+  //U
+  updateCandidatePosition(id: any, data: any): Observable<any> {
+    return this.httpClient.put(`${apiUrl}/${id}`, data);
+  }
+
+  //D
+  deleteCandidatePosition(id: any): Observable<any> {
+    return this.httpClient.delete(`${apiUrl}/${id}`);
+  }
+>>>>>>> master
 }
