36d92de6623174b5df8dcb911b4294ddf55c8973
Marc Lopez <marclopezmarco469@gmail.com>
2022-06-27 10:33:56 +0200

    select tablas intermedias, y mostrar candidaturas fijas

--- src/app/admin/components/formularios/form-candidate-position/form-candidate-position.component.ts
+++ src/app/admin/components/formularios/form-candidate-position/form-candidate-position.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidateService } from 'src/app/admin/services/s-candidate/candidate.service';
+import { PositionService } from 'src/app/admin/services/s-position/position.service';
 
 @Component({
   selector: 'app-form-candidate-position',
@@ -6,15 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-candidate-position.component.css'],
 })
 export class FormCandidatePositionComponent implements OnInit {
-  constructor() {}
+  constructor(private candidateService: CandidateService,private positionService: PositionService) {}
+  positions: any;
+  candidates: any;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.listCandidates();
+    this.listPosition();
+
+
+  }
 
   fkCandidate: any;
   fkPosition: any;
 
-  validarFkCandidate: boolean = true;
-  validarFkPosition: boolean = true;
+  validarFkCandidate: boolean = false;
+  validarFkPosition: boolean = false;
+
+  //get all
+  listCandidates() {
+    //Llamas al sevice de listar y pasa la lista a la variable
+    this.candidateService.listCandidates().subscribe((candidates: any) => {
+      this.candidates = candidates;
+      console.log(this.candidates)
+    });
+  }
+
+  //get all
+  listPosition() {
+    //Llamas al service de listar y pasa la lista a la variable
+    this.positionService.listPosition().subscribe((positions: any) => {
+      this.positions = positions;
+      console.log(this.positions)
+
+    });
+  }
+
 
   enviarFormulario() {}
 }
--- src/app/admin/components/formularios/form-hr-position/form-hr-position.component.ts
+++ src/app/admin/components/formularios/form-hr-position/form-hr-position.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HrUserService } from 'src/app/admin/services/s-hr-user/hr-user.service';
+import { PositionService } from 'src/app/admin/services/s-position/position.service';
 
 @Component({
   selector: 'app-form-hr-position',
@@ -6,15 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-hr-position.component.css'],
 })
 export class FormHrPositionComponent implements OnInit {
-  constructor() {}
+  hrUsers: any;
+  positions: any;
 
-  ngOnInit(): void {}
+
+  constructor(private hrUserService: HrUserService,private positionService: PositionService) {}
+
+  ngOnInit(): void {
+    this.listHrUser();
+    this.listPosition();
+
+  }
 
   fkHrUser: any;
   fkPosition: any;
 
-  validarFkHrUser: boolean = true;
-  validarFkPosition: boolean = true;
+  validarFkHrUser: boolean = false;
+  validarFkPosition: boolean = false;
+
+
+     // get all
+  listHrUser() {
+    // llamas al service de listar y pasa la lista a la variable
+    this.hrUserService.listHrUser().subscribe((hrUsers: any) => {
+      this.hrUsers = hrUsers;
+      console.log(this.hrUsers);
+
+    });
+  }
+
+    //get all
+    listPosition() {
+      //Llamas al service de listar y pasa la lista a la variable
+      this.positionService.listPosition().subscribe((positions: any) => {
+        this.positions = positions;
+        console.log(this.positions);
+
+      });
+    }
+
 
   enviarFormulario() {}
 }
